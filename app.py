6bae7a34e8df8c383ff1c5a90b5fa76ffe92fcb3
Satwik garg <satwik22461@iiitd.ac.in>
2024-03-28 14:17:08 +0530

    customer page done register page done but the database is not changing

--- app.py
+++ app.py
@@ -1,19 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from database import register,dispaly
 
 app = Flask(__name__)
 
 @app.route('/')
 @app.route("/deller")
-def home():
-    return render_template('distributor_home.html')
+def deller_home():
+    return render_template('distributor_home.html',home='deller')
 
-@app.route("/register_customer")
-def register_customer():
-    return render_template('register_customer.html')
+@app.route("/customer")
+def customer_home():
+    return render_template('customer_home.html',home='customer')
 
-@app.route("/register_distributor")
+
+@app.route("/register",methods=['GET'])
 def register_distributor():
-    return render_template('register_distributor.html')
+    return render_template('register.html')
+
+@app.route('/submit-register', methods=['POST'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form
+        register(data)
+        dispaly()
+        return f"Form submitted with data: {data}"
 
 if(__name__ == "__main__"):
     app.run(host="0.0.0.0",debug = True)
--- database.py
+++ database.py
@@ -2,12 +2,32 @@ import urllib.parse
 import sqlalchemy
 from sqlalchemy import create_engine, text
 
-encoded_password = urllib.parse.quote_plus("root18")
+encoded_password = urllib.parse.quote_plus("Satwik@07")
 
 connection_string = f"mysql+pymysql://root:{encoded_password}@localhost:3306/myntra"
 
 engine = create_engine(connection_string)
 
-with engine.connect() as conn:
-    result = conn.execute(text("SELECT * FROM customer"))
-    print(result.fetchall())  
+
+
+def register(data):
+    with engine.connect() as conn:
+        name = data.get("username")
+        email = data.get("email")
+        phone = data.get("Phone")
+        address_line1 = data.get("AddressLine1")
+        city = data.get("City")
+        state = data.get("State")
+        postal_code = data.get("Pincode")
+        country = data.get("Country")
+        query = text('INSERT INTO customer (name, address_line1, city, state, postal_code, country) VALUES (:name, :address_line1, :city, :state, :postal_code, :country)')
+        result = conn.execute(query, {'name': name, 'address_line1': address_line1, 'city': city, 'state': state, 'postal_code': postal_code, 'country': country})
+        result1 = conn.execute(text("SELECT * FROM customer"))
+        print(result1.fetchall())
+
+
+def dispaly():
+    with engine.connect() as conn:
+        result = conn.execute(text("SELECT * FROM customer"))
+        print(result.fetchall())  
+
